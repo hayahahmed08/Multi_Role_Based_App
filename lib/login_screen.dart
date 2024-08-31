5fd26a87183b2508f9fa4f558202c07111e5958a
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-08-31 22:10:33 +0500

    shared preferences

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class loginScreen extends StatelessWidget {
   const loginScreen({super.key});
@@ -11,7 +12,12 @@ class loginScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           InkWell(
-            onTap: () {
+            onTap: () async {
+              SharedPreferences sp = await SharedPreferences.getInstance();
+              sp.setString('name', 'hayah');
+              sp.setInt('age', 20);
+              print(sp.getString("name"));
+
               print('tap');
               add(2, 4);
               func();
@@ -30,12 +36,13 @@ class loginScreen extends StatelessWidget {
     );
   }
 
-  void add(int x, int y) { 
-    print(x + y);
+  Future<void> func() async {
+    // Wait until you get your desired result
+    await Future.delayed(Duration(seconds: 10));
+    print("delayed");
   }
+}
 
-  Future<void> func() async{ //wait until u get ur desired result
-   await Future.delayed(Duration(seconds: 10));
-   print("delayed");
-  }
+void add(int x, int y) {
+  print(x + y);
 }
